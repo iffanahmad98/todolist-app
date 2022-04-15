a2fbdd261c0459e89b9147a355faa01026ff9e81
iffanahmad98 <iffanahmad98@gmail.com>
2022-04-15 17:06:31 +0700

    create function useState

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,24 +1,25 @@
 import './Todo.css';
+import { useState } from 'react';
 import TodoList from '../todo-list/TodoList';
 import TodoCreate from '../todo-create/TodoCreate';
 
 const Todo = () => {
-  const todos = [
+  const [getTodos, setTodos] = useState([
     { id: 1, title: 'Eat' },
     { id: 2, title: 'Sleep' },
     { id: 3, title: 'Code' },
-  ];
+  ]);
 
   const eventCreateTodo = (todo) => {
-    todos.push(todo);
-    console.log(todos);
+    setTodos(getTodos.concat(todo));
+    console.log(getTodos);
   };
 
   return (
     <div>
       <h3>Todo List</h3>
       <TodoCreate onCreateTodo={eventCreateTodo} />
-      <TodoList dataTodos={todos} />
+      <TodoList dataTodos={getTodos} />
     </div>
   );
 };
