52ff5dc8d9cff092dc01c1ee57edc40129810a74
iffanahmad98 <iffanahmad98@gmail.com>
2022-06-18 19:28:19 +0700

    Show data

--- src/components/todo-create/TodoCreate.js
+++ src/components/todo-create/TodoCreate.js
@@ -1,21 +1,29 @@
 import './TodoCreate.css';
+import { useState } from 'react';
 
 const TodoCreate = (props) => {
+  const [getInputTodo, setInputTodo] = useState('');
   const handleSubmit = (event) => {
     event.preventDefault();
 
     const newTodo = {
       id: Math.floor(Math.random() * 100) + 1,
-      title: 'Learn react',
+      title: getInputTodo,
     };
 
     props.onCreateTodo(newTodo);
 
     // console.log(newTodo);
   };
+
+  const handleInputTodo = (event) => {
+    setInputTodo(event.target.value);
+    console.log(getInputTodo);
+  };
+
   return (
     <form className="todo-form" onSubmit={handleSubmit}>
-      <input type="text" />
+      <input type="text" onChange={handleInputTodo} />
       <button type="submit">Add</button>
     </form>
   );
